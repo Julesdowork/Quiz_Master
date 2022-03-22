e4931f9bc1268243f022bd481d7c77a9671306c9
Julian McNeill <julesdowork@gmail.com>
2022-03-22 18:13:07 -0400

    For Loops

--- Quiz_Master/Assets/Scripts/Quiz.cs
+++ Quiz_Master/Assets/Scripts/Quiz.cs
@@ -7,9 +7,16 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
     }
 }
